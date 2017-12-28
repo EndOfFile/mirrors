837b6ac1f53ad341103a785b378167025b70af1d
EndOfFile <EndOfFile.org@gmail.com>
2017-12-28 13:26:37 +0100

    Added js-beautify formatting config and applied to MangaFox

--- MangaFox.js
+++ MangaFox.js
@@ -9,29 +9,25 @@ var MangaFox = {
     },
     getMangaList: function(search, callback) {
         "use strict";
-        var urlManga = "http://mangafox.la/search.php?name=" + search +
-            "&advopts=1";
+        var urlManga = "http://mangafox.la/search.php?name=" + search + "&advopts=1";
         $.ajax({
             url: urlManga,
             beforeSend: function(xhr) {
-                xhr.setRequestHeader("Cache-Control",
-                    "no-cache");
+                xhr.setRequestHeader("Cache-Control", "no-cache");
                 xhr.setRequestHeader("Pragma", "no-cache");
             },
             success: function(objResponse) {
                 var div = document.createElement("div");
                 div.innerHTML = objResponse.replace(/<img/gi, '<noload');
-                if (objResponse.indexOf("No Manga Series") !==
-                    -1) {
+                if (objResponse.indexOf("No Manga Series") !== -1) {
                     callback("Manga-Fox", []);
                 } else {
                     var res = [];
-                    $("#listing tr td:first-child a", div).each(
-                        function(index) {
-                            res[index] = [$(this).html(),
-                                $(this).attr("href")
-                            ];
-                        });
+                    $("#listing tr td:first-child a", div).each(function(index) {
+                        res[index] = [$(this).html(),
+                            $(this).attr("href")
+                        ];
+                    });
                     callback("Manga-Fox", res);
                 }
             }
@@ -42,8 +38,7 @@ var MangaFox = {
         $.ajax({
             url: urlManga + "?no_warning=1",
             beforeSend: function(xhr) {
-                xhr.setRequestHeader("Cache-Control",
-                    "no-cache");
+                xhr.setRequestHeader("Cache-Control", "no-cache");
                 xhr.setRequestHeader("Pragma", "no-cache");
             },
             success: function(objResponse) {
@@ -51,44 +46,19 @@ var MangaFox = {
                 objResponse = objResponse.replace(/<img\b[^>]*>/ig, ''); //avoid loading cover image
                 div.innerHTML = objResponse;
                 var res = [];
-                var mangaName = $('#title h2', div).text().substr(
-                    5, $('#title h2', div).text().length -
-                    18);
-                $("ul.chlist h3, ul.chlist h4", div).each(
-                    function(index) {
-                        if ($('a', $(this)).attr("href")
-                            .indexOf("/manga/") !== -1) {
-                            var vol = $(this).parents(
-                                    'ul.chlist').prev(
-                                    'div.slide').children(
-                                    'h3').contents(
-                                    ':not(span)').text()
-                                .trim().substr(7);
-                            var tit = 'Vol ' + vol +
-                                ' Ch ' + $('a', $(this))
-                                .text().substr(
-                                    mangaName.length +
-                                    1) + ': ' + $(
-                                    'span.title', $(
-                                        this)).text();
-                            var url = $('a', $(this)).attr(
-                                "href");
-                            var curChapURL = url.substr(
-                                0, url.lastIndexOf(
-                                    "/") + 1);
-                            if (curChapURL.substr(
-                                curChapURL.length -
-                                2, 2) === "//") {
-                                curChapURL = curChapURL
-                                    .substr(0,
-                                        curChapURL.length -
-                                        1);
-                            }
-                            res[res.length] = [tit.trim(),
-                                "http:" + curChapURL + "1.html"
-                            ];
+                var mangaName = $('#title h2', div).text().substr(5, $('#title h2', div).text().length - 18);
+                $("ul.chlist h3, ul.chlist h4", div).each(function(index) {
+                    if ($('a', $(this)).attr("href").indexOf("/manga/") !== -1) {
+                        var vol = $(this).parents('ul.chlist').prev('div.slide').children('h3').contents(':not(span)').text().trim().substr(7);
+                        var tit = 'Vol ' + vol + ' Ch ' + $('a', $(this)).text().substr(mangaName.length + 1) + ': ' + $('span.title', $(this)).text();
+                        var url = $('a', $(this)).attr("href");
+                        var curChapURL = url.substr(0, url.lastIndexOf("/") + 1);
+                        if (curChapURL.substr(curChapURL.length - 2, 2) === "//") {
+                            curChapURL = curChapURL.substr(0, curChapURL.length - 1);
                         }
-                    });
+                        res[res.length] = [tit.trim(), "http:" + curChapURL + "1.html"];
+                    }
+                });
                 callback(res, obj);
             }
         });
@@ -96,8 +66,7 @@ var MangaFox = {
     getInformationsFromCurrentPage: function(doc, curUrl, callback) {
         "use strict";
         var str = $('#series > strong a', doc).text(); // dom lookups are expensive!
-        var name = $('#related > h3 a', doc).text() || str.substring(0,
-            str.length - 6); //falls through #related, into #series
+        var name = $('#related > h3 a', doc).text() || str.substring(0, str.length - 6); //falls through #related, into #series
         var currentChapter = $("#series h1", doc).text();
         var url = curUrl;
         var posSl5 = 0;
@@ -121,8 +90,7 @@ var MangaFox = {
         var res = [];
         $('#top_bar select.m option', doc).each(function() {
             if (this.value > 0) {
-                res.push(curUrl.substr(0, curUrl.lastIndexOf(
-                    "/") + 1) + this.value + '.html');
+                res.push(curUrl.substr(0, curUrl.lastIndexOf("/") + 1) + this.value + '.html');
             }
         });
         return res;
@@ -158,8 +126,7 @@ var MangaFox = {
         });
         $("#image", doc).remove();
         $("#tool", doc).next().remove();
-        $("#viewer", doc).after("<div class='navAMR'></div>").before(
-            "<div class='navAMR'></div>");
+        $("#viewer", doc).after("<div class='navAMR'></div>").before("<div class='navAMR'></div>");
         $(".widepage.page", doc).remove();
         $('.fb_iframe_widget', doc).remove();
         if (typeof doc.createElement === 'function') {
